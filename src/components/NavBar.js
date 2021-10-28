0b2006636f66f95128a79997a149b006ae17d6d1
nascott <nascott94@outlook.com>
2021-10-28 16:10:16 -0600

    started on home component and added fonts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ const Container = styled.div`
   height: 100px;
   background-color: none;
   display: flex;
-  border-bottom: 1px solid #333333;
+  border-bottom: 2px solid #333333;
 `;
 
 const Left = styled.div`
@@ -24,6 +24,11 @@ const Right = styled.div`
   align-items: center;
   padding: 20px;
 `;
+const Logo = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  letter-spacing: -0.5rem;
+`;
 
 const MenuItem = styled.div`
   cursor: pointer;
@@ -35,7 +40,9 @@ const MenuItem = styled.div`
 export const NavBar = () => {
   return (
     <Container>
-      <Left>ND</Left>
+      <Left>
+        <Logo>ND</Logo>
+      </Left>
       <Right>
         <MenuItem>Services</MenuItem>
         <MenuItem>About</MenuItem>
