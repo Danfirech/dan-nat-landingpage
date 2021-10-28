daabce67208edddaddd0a240e894d8f690b11470
Dan Possehl <danielbpossehl@gmail.com>
2021-10-28 16:54:15 -0600

    added contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: darkblue;
+`;
+
+export const Contact = () => {
+  return <Container></Container>;
+};
