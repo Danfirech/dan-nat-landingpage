d13892300cf5e2a23dbb1a83f111c6be200a5947
Dan Possehl <danielbpossehl@gmail.com>
2021-10-28 14:55:39 -0600

    here we go

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  background-color: lightblue;
+`;
+
+function App() {
+  return <Container></Container>;
+}
+
+export default App;
